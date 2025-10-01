b3a0bc16fc7cd8ab41d45e7edf1184a07216a9d6
amitkrjaiswal <amitjaiswal45454545@gmail.com>
2025-10-02 00:15:09 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+
+
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Navbar</title>
+	<style>
+		body {
+			margin: 0;
+			font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+			background-color: #f9f9f9;
+		}
+
+		h1 {
+			font-size: 40px;
+			background: linear-gradient(90deg, #ff4e50, #f9d423);
+			text-align: center;
+			color: white;
+			padding: 20px 0;
+			box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+			margin: 0;
+			border-radius: 0 0 10px 10px;
+		}
+	</style>
+</head>
+<body>
+
+<h1> Online Marks Management System</h1>
+
+</body>
+</html>
